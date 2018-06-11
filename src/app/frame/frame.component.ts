02c7bf16af84fa21151c56c10382398844b41d14
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-06-11 15:57:33 -0400

    house keeping

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-frame',
   templateUrl: './frame.component.html',
   styleUrls: ['./frame.component.scss'],
-  providers:  [ ]
+  providers:  []
 })
 export class FrameComponent implements OnInit {
-
+  api: string = "";
   constructor() { }
 
   ngOnInit() {
-
   }
 
 }
