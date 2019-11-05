18b4ed74d684bda1dc9645b92e957af3a85494a8
JohnsonLu3 <JohnsonLu94@gmail.com>
2019-11-05 16:05:56 -0500

    added temp dev file to hold api key and ignored it in gitignore

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '../../scss/App.scss';
 import Frame from './Frame'
 import InfoModal from './InfoModal'
+import API from '../../Api'
+
+let getDogs = "https://api.petfinder.com/v2/animals?type=dog"
 
 function Main() {
   return (
@@ -9,8 +12,15 @@ function Main() {
       <Frame />
       <InfoModal isOpen="false" image="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/46484621/1/?bust=1572982306"/>
       <div id="backgroundImage"></div>
+      {printAPI()}
     </main>
   );
+}
+
+function printAPI(){
+  let api = new API();
+  console.log(api.getAPI().token);
+  return;
 }
 
 export default Main;
