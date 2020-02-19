8adfd0c3bd1b44d35d5bd30861dab6259e20cca1
Johnson Lu <JohnsonLu94@gmail.com>
2020-02-19 11:16:06 -0500

    Did some clean up and restructing

--- Node_API/index.js
+++ Node_API/index.js
@@ -1,14 +1,4 @@
-const http = require("http")
-const fs = require("fs")
-const path = require("path")
-const EventEmitter = require("events")
-const Petfrinder = require("./Petfinder")
-const PORT = process.env.PORT || 5000
-const API_KEY = process.env.API_PETFINDER
 const express = require('express');
-
-const petFinder = new Petfrinder(API_KEY);
-petFinder.getPets();
 
 const PORT = process.env.PORT || 5000
 
@@ -16,11 +6,3 @@ const app = express();
 app.listen(PORT, ()=>{
     console.log(`Server listening on Port ${PORT}`)
 })
-
-
-// const server = http.createServer((req, res) => {
-//     if (req.url === "/") {
-//         getIndex(req, res)
-//     }
-// });
-// server.listen(PORT, () => { console.log("listing on port", PORT) })
