24808112ec6cac934a8bc6095b07650cbaac9fc0
Johnson Lu <JohnsonLu94@gmail.com>
2020-02-19 12:27:00 -0500

    api now connects to petfinder api v2 instead of 1

--- Node_API/controller/PetfinderController.js
+++ Node_API/controller/PetfinderController.js
@@ -1,70 +1,28 @@
-const axios = require('axios');
+const axios = require("axios");
+var petfinder = require("@petfinder/petfinder-js");
 
 class Petfinder {
+  constructor() {
+    this.clientId = process.env.API_PETFINDER;
+    this.clientSecret = process.env.API_PETFINDER_SECRET;
+    this.limit = 25;
+    this.client = new petfinder.Client({
+      apiKey: this.clientId,
+      secret: this.clientSecret
+    });
 
+    console.log(this.clientId)
+    console.log(this.clientSecret)
 
-    /****
-     * TODO!!!!!
-     * Adjust api calls from version 1 to version 2
-     * version 2 is a lot cleaner and better in their results sets
-     * it also handles images a lot better
-     * 
-     * But version 2 requires OAuth2
-     * So find a way to generate valid tokens
-     */
+    this.client.animal
+      .search()
+      .then(function(response) {
+        console.log(response.data.animals);
+      })
+      .catch(function(err) {
+        throw err
+      });
+  }
 
-    constructor(API) {
-        this.API = API
-        this.testMethod = 'http://api.petfinder.com/pet.getRandom?key=' + this.API + '&output=full&format=json';
-        this.prefix = "http://api.petfinder.com/v2/";
-        this.suffix = '?key=' + this.API + '&output=full&format=json'
-
-        this.limit = 25;
-    }
-
-    getPets() {
-        const url = `${this.prefix}pet.getRandom${this.suffix}&count=${this.limit}&animal=dog`;
-        let pets = [];
-        axios.get(url)
-            .then(res => {
-                const data = res.data;
-                for (let pet of data.petfinder.pet) {
-                    console.log(pet.id.$t)
-                    if (pet.media.photos.photo.length > 0) {
-                        pets.push(pet);
-                    }
-                }
-            })
-            .catch(err => { throw err })
-    }
-
-    getPetsByCount(count) {
-        const url = `${this.prefix}animals&limit=${count}`
-        axios.get(url)
-            .then(res => {
-                const data = res.data;
-                // TO DO
-            })
-    }
-
-    getPetById(id) {
-        const url = ``
-        axios.get(url)
-            .then( res=>{
-                const data = res.data;
-            })
-            .catch(err => { throw err })
-    }
-
-    // Old Test call for Version 1
-    // testAPICall() {
-    //     express(this.testMethod)
-    //         .then(res => {
-    //             console.log(res.data);
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //         });
-    // }
 }
-module.exports = Petfinder
+module.exports = Petfinder;
--- Node_API/index.js
+++ Node_API/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const PORT = process.env.PORT || 5000
 
+const Petfinder = require("./controller/PetfinderController");
+let pf = new Petfinder();
 const app = express();
 app.listen(PORT, ()=>{
     console.log(`Server listening on Port ${PORT}`)
 })
+ 
